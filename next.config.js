b0cc5137c66acf77b965c337cbca59c3f3caea1b
pham vu thuoc <phamvuthuoc@gmail.com>
2022-05-31 18:05:31 +0700

    feat:  fix lôi đẩy tin

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000/api' // development api
+      ? 'http://202.92.6.221:3000/api' // development api
       : 'http://202.92.6.221:3000/api' // production api
   },
   webpack: (config, { isServer }) => {
