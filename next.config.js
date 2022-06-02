077ff4849972e851f8c19f7d77165997c2b53b3d
pham vu thuoc <phamvuthuoc@gmail.com>
2022-06-02 17:46:19 +0700

    feat: tao api login

--- next.config.js
+++ next.config.js
@@ -4,14 +4,17 @@ const nextConfig = {
   serverRuntimeConfig: {
     secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
   },
+  secretPass: {
+    secret: 'kslajdlajscasaajskwaewas'
+  },
   accountViettel: {
     account: '0982288550',
     pass:'Pthp123@'
   },
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
-      ? 'http://202.92.6.221:3000/api' // development api
-      : 'http://202.92.6.221:3000/api' // production api
+    ? 'http://202.92.6.221:3000/api' // development api
+    : 'http://202.92.6.221:3000/api' // production api
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
